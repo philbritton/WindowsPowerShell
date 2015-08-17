725f23bf94957bcd4420bf410e275a2d71d13473
Phil Britton <pbritton@dacgroup.com>
2015-08-17 09:12:15 -0400

    added history

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -38,6 +38,7 @@ Function Test-RegistryValue
         }
     }
 }
+##Disable UAC Function##
 Function Disable-UAC
 {
     $EnableUACRegistryPath = "REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System"
@@ -52,6 +53,14 @@ Function Disable-UAC
         New-ItemProperty -Path $EnableUACRegistryPath -Name $EnableUACRegistryKeyName -Value 0 -PropertyType "DWord"
     }
 }
+###History Function###
+$HistoryFilePath = Join-Path ([Environment]::GetFolderPath('UserProfile')) .ps_history
+Register-EngineEvent PowerShell.Exiting -Action { Get-History | Export-Clixml $HistoryFilePath } | out-null
+if (Test-path $HistoryFilePath) { Import-Clixml $HistoryFilePath | Add-History }
+# if you don't already have this configured...
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
 ##########################################Aliases#########################################################
 function subl { &"${Env:ProgramFiles}\Sublime Text 3\sublime_text.exe" $args }
 ############################Custom Sourcing########################################
